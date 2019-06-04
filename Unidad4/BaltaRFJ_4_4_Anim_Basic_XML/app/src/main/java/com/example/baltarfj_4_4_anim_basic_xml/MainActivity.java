80e2b4a90cecb7c7d9bf98a46f6f120ed6c97b0d
Francisco Javier Baltazar Ramirez <frjabaltazarra@ittepic.edu.mx>
2019-06-03 23:30:54 -0600

    U4 44

---
+++ Unidad4/BaltaRFJ_4_4_Anim_Basic_XML/app/src/main/java/com/example/baltarfj_4_4_anim_basic_xml/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.baltarfj_4_4_anim_basic_xml;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
